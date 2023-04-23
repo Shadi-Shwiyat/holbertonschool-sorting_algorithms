e52469d2fc94da893ee5933afeb14b9c292cfecb
Shadi-Shwiyat <raz611103@gmail.com>
2023-04-23 16:02:03 -0700

    Function sorts a list using insertion algorithm

--- sort.h
+++ sort.h
@@ -21,6 +21,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void swap(listint_t *current, listint_t *previous);
+listint_t *create_listint(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
